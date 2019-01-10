c4617ef1c684104db991a6bb83af569e5013da3a
raymillward <ray_millward@hotmail.com>
2019-01-10 21:16:15 +0000

    Started adding a main picture area to the homepage

--- src/homepage/HomePage.js
+++ src/homepage/HomePage.js
@@ -1,9 +1,27 @@
 import React from 'react'
+import { withStyles } from '@material-ui/core/styles';
+import backgroundImage from '../images/background.jpg'
 
+const styles = {
+  root: {
+    marginTop: 14,
+    backgroundColor: 'red'
+  },
+  backgroundImage: {
+    height: '100vh'
+  },
+  overlayText: {
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)'
+  }
+}
 const HomePageComponent = (props) => (
-  <div>
-    <div>Hello World</div>
+  <div className={props.classes.root}>
+    <img className={props.classes.backgroundImage} src={backgroundImage} alt="Background image" />
+    <div className={props.classes.overlayText}>A home for all</div>
   </div>
 )
 
-export default HomePageComponent
+export default withStyles(styles)(HomePageComponent)
